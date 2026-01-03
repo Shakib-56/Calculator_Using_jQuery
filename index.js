8042ee932055f274ec247d5a7e3509ae2e0d166e
Shakib <hossenmdshakib47@gmail.com>
2026-01-04 03:02:09 +0600

    clear functionality added

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function insertNumber(num) {
   let existingNumbers = $("#result").val();
   $("#result").val(existingNumbers + num);
 }
+
+// clear result function
+function clearResult() {
+  $("#result").val("");
+}
